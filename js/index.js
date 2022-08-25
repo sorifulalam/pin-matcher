f9380a99d4c49aa00b9b570f0578b834bbf81bff
Soriful Alam <58595744+sorifulalam@users.noreply.github.com>
2022-08-26 03:21:19 +0600

    upp

---
+++ js/index.js
@@ -0,0 +1,27 @@
+
+
+let displayCatch = document.getElementById('displayCatch')
+
+function genaratePin(){
+    let genarate = Math.round(Math.random() * 10000);
+    document.getElementById("displayCatch").value=" "
+    displayCatch.value += genarate
+   
+    
+    return genarate   
+    
+}
+// document.getElementById('displayCatch').value=" "
+
+// function getPin(){
+//     let pin = genaratePin()
+    
+//     let clearPin = pin +'';
+//     if(clearPin.length===4){
+//         return pin
+//     }
+//     else{
+//         return getPin()
+//     }
+// }
+
