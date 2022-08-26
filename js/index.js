0b36e6fd56a8dc093b3bdb8f5da0f5baacb693df
Soriful Alam <58595744+sorifulalam@users.noreply.github.com>
2022-08-26 23:10:36 +0600

    upp

--- js/index.js
+++ js/index.js
@@ -23,3 +23,40 @@ function getPin(){
     }
 }
 
+let displayNumber =document.getElementById('displayNumber')
+
+function digitNumber(digit){
+    displayNumber.value +=digit
+}
+
+function digitClear(){
+    displayNumber.value=" "
+}
+
+function digitDlet(){
+    displayNumber.value = displayNumber.value.slice(0,-1)
+}
+
+function clickNumber(){
+    let getPin = document.getElementById('displayCatch')
+   let curentPin=getPin.value;
+
+    let displayNumber =document.getElementById('displayNumber')
+   let typedNumber = displayNumber.value;
+
+    let notSuccesMessage =document.getElementById('notSuccesMessage')
+
+    let successMessage = document.getElementById('successMessage')
+
+    if(typedNumber ===curentPin){
+        successMessage.style.display='block'
+        notSuccesMessage.style.display='none'
+    }
+    else{
+        
+        notSuccesMessage.style.display='block'
+        successMessage.style.display='none'
+    }
+
+    
+}
